c8fce2d3265def63adb1a3eb177c4e869f134f98
RupendraCHA <chandaluri210@gmail.com>
2025-03-10 01:51:27 +0530

    Creating Login User API endpoint

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -20,7 +20,32 @@ module.exports.registerUser = async (req, res, next) => {
         password: hashedPassword
     })
 
+    const token = await user.generateAuthToken()
+
+    res.status(201).json({token, user, isTrue})
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const {email, password} = req.body
+
+    const user = await userModel.findOne({email}).select('+password')
+
+    if (!user) {
+        return res.status(401).json({message: "Invalid email or Password"})
+    }
+
+    const isMatch = await user.comparePassword(password)
+
+    if (!isMatch) {
+        return res.status(401).json({message: "Invalid email or Password"})
+    }
+
     const token = user.generateAuthToken()
 
-    res.status(201).json({token, user})
+    res.status(200).json({token, user})
 }
