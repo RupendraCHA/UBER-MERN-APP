4644e5e4aacd17ea0b12384905b5cfb2d4c7b774
RupendraCHA <chandaluri210@gmail.com>
2025-04-20 10:32:27 +0530

    merging

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -28,8 +28,6 @@ const UserSignup = () => {
       password: password
     }
 
-    
-
     const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
 
     if (response.status === 201){
