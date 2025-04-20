112d0e674b4889a8ac0030307565f7b30784f3af
RupendraCHA <chandaluri210@gmail.com>
2025-04-20 10:26:53 +0530

    Changing branch

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -28,6 +28,8 @@ const UserSignup = () => {
       password: password
     }
 
+    
+
     const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
 
     if (response.status === 201){
